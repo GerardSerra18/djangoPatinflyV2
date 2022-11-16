fb615a7e5f0f3e99df2ceaed7df67f8c96433285
Gerard Serra <100229800+GerardSerra18@users.noreply.github.com>
2022-11-16 19:17:05 +0100

    clean

--- djangoPatinflyV2/settings.py
+++ djangoPatinflyV2/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ml2+1^)%j&f9*ytqe26@(*)1z*#1%*nguj$h1mf1p9ij#&2j98
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['patinfly-v2.herokuapp.com']
+ALLOWED_HOSTS = ['patinfly-v2.herokuapp.com', "127.0.0.1"]
 
 
 # Application definition
